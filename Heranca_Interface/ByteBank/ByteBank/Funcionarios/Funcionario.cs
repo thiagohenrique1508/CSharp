95935d1d9611a79d0489bd03041a00205db19c08
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-01-05 16:57:22 -0300

    Commit Final

---
+++ Heranca_Interface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+
+    }
+}
